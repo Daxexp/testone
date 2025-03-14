0ef8841015f2e9b8b50876d1dbaf2792cbaf77ee
Daxexp <exp12mt@gmail.com>
2025-03-15 03:11:45 +0530

    Update serve_stream.php

--- serve_stream.php
+++ serve_stream.php
@@ -3,7 +3,7 @@
 
 // Define a list of channels with their m3u8 links
 $channels = [
-    "channel1" => "https://edge2-moblive.yuppcdn.net/trans1sd/smil:strtml19.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMTo0NjoxMiBBTSZoYXNoX3ZhbHVlPU9tZmExdkd1N3R3TjN0QkdBT21hT3c9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfMTlfLTEmc3RybV9sZW49Mjc=",
+    "channel1" => "https://edge3-moblive.yuppcdn.net/transsd/smil:sirtv09.smil/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MDMvMTAvMjAyNCAxMToxMzo1MyBBTSZoYXNoX3ZhbHVlPWxTdVZPMHpONjVtTHkzTS9MWjkvUEE9PSZ2YWxpZG1pbnV0ZXM9NSZpZD15dXBwdHZvdHRfNV8yMDE3NzBfYTA0NmIxNGUtYzBiNS1mOTAzLTgzZjUtZDE3YzY0MmE1YTFjX0xLXzE3NS4xNTcuMTM2LjkwX3NsdF8xX2NoYW5uZWxfOV8tMSZzdHJtX2xlbj0yNQ==",
     "channel2" => "https://example.com/channel2.m3u8",
     "channel3" => "https://example.com/channel3.m3u8"
 ];
