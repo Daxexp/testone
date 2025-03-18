9aaac541264ccf44a1b3856a8e9f01ad270f593d
Daxexp <exp12mt@gmail.com>
2025-03-19 00:06:22 +0530

    Add files via upload

---
+++ download_video.py
@@ -0,0 +1,17 @@
+import sys
+import yt_dlp
+
+def download_video(url):
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': '%(title)s.%(ext)s',
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python download_video.py <youtube_video_url>")
+    else:
+        download_video(sys.argv[1])
